507ef2d78461fe21d17d39a42fd8e78debcd142a
chengpan <1834022721@qq.com>
2023-04-24 23:18:11 +0800

    first commit 'ip_web'

--- lib/login/components/login_screen.dart
+++ lib/login/components/login_screen.dart
@@ -1,7 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:ip_broadcast_web/login/components/login_screen.dart';
+import 'package:ip_broadcast_web/login/responsive.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -15,139 +19,136 @@ var _isObsure = true;
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    Size screenSize = MediaQuery.of(context).size;
-    Orientation orientation = MediaQuery.of(context).orientation;
-    print(screenSize);
     return Scaffold(
       body: MyWidget(),
     );
   }
 }
 
+// ignore: must_be_immutable
 class MyWidget extends StatelessWidget {
   Widget mobileWidget = Column(children: [
     Expanded(
       flex: 1,
       child: Container(
         height: double.infinity,
-        child: Row(
-          children: [
-            Expanded(
-              child: Column(
-                children: [
-                  SizedBox(height: 120),
-                  Container(
-                    height: 6,
-                    color: Color(0xFFCCCCCC),
-                  ),
-                  SizedBox(height: 96.2),
-                  Container(
-                    width: 396.58,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Row(
-                          children: [
-                            SizedBox(width: 15.19),
-                            SvgPicture.asset(
-                              'assets/svg/icon_logo.svg',
-                              width: 55.42,
-                              height: 52.31,
-                            ),
-                            SizedBox(width: 11.65),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  'Audio Frequency',
-                                  style: TextStyle(
-                                    color: Color(0xFFF5F5F5),
-                                    fontSize: 20,
+        child: SingleChildScrollView(
+          child: Expanded (
+            child: Column(
+              children: [
+                SizedBox(height: 120),
+                Container(
+                  height: 6,
+                  color: Color(0xFFCCCCCC),
+                ),
+                SizedBox(height: 96.2),
+                Container(
+                  width: 396.58,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Row(
+                        children: [
+                          SizedBox(width: 15.19),
+                          SvgPicture.asset(
+                            'assets/svg/icon_logo.svg',
+                            width: 55.42,
+                            height: 52.31,
+                          ),
+                          SizedBox(width: 11.65),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Audio Frequency',
+                                style: TextStyle(
+                                  color: Color(0xFFF5F5F5),
+                                  fontSize: 20,
+                                ),
+                              ),
+                              SizedBox(height: 2),
+                              Text(
+                                'Control  System',
+                                style: TextStyle(
+                                  color: Color(0xFF7E8A94),
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                      SizedBox(height: 20.78),
+                      Container(
+                        width: 396.58,
+                        height: 349.62,
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(42, 33, 42, 43),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                '欢迎回来，请登录您的帐户',
+                                style: TextStyle(
+                                    color: Color(0xFFA5A6AD), fontSize: 16),
+                              ),
+                              SizedBox(height: 23.66),
+                              TextFormField(
+                                decoration: InputDecoration(
+                                  label: Text(
+                                    '账号',
+                                    style: TextStyle(
+                                        color: Color(0xFFBBBBBB),
+                                        fontSize: 16),
                                   ),
+                                  hintText: '请输入账号',
                                 ),
-                                SizedBox(height: 2),
-                                Text(
-                                  'Control  System',
-                                  style: TextStyle(
-                                    color: Color(0xFF7E8A94),
-                                    fontSize: 14,
+                              ),
+                              SizedBox(height: 38),
+                              TextFormField(
+                                obscureText: _isObsure,
+                                inputFormatters: [
+                                  FilteringTextInputFormatter(
+                                    RegExp("^[a-z0-9A-Z]+"),
+                                    allow: true,
                                   ),
-                                ),
-                              ],
-                            )
-                          ],
-                        ),
-                        SizedBox(height: 20.78),
-                        Container(
-                          width: 396.58,
-                          height: 349.62,
-                          child: Padding(
-                            padding: const EdgeInsets.fromLTRB(42, 33, 42, 43),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  '欢迎回来，请登录您的帐户',
-                                  style: TextStyle(
-                                      color: Color(0xFFA5A6AD), fontSize: 16),
-                                ),
-                                SizedBox(height: 23.66),
-                                TextFormField(
-                                  decoration: InputDecoration(
+                                ],
+                                decoration: InputDecoration(
                                     label: Text(
-                                      '账号',
+                                      '密码',
                                       style: TextStyle(
                                           color: Color(0xFFBBBBBB),
                                           fontSize: 16),
                                     ),
-                                    hintText: '请输入账号',
-                                  ),
-                                ),
-                                SizedBox(height: 38),
-                                TextFormField(
-                                  obscureText: _isObsure,
-                                  inputFormatters: [
-                                    FilteringTextInputFormatter(
-                                      RegExp("^[a-z0-9A-Z]+"),
-                                      allow: true,
-                                    ),
-                                  ],
-                                  decoration: InputDecoration(
-                                      label: Text(
-                                        '密码',
-                                        style: TextStyle(
-                                            color: Color(0xFFBBBBBB),
-                                            fontSize: 16),
+                                    hintText: '请输入密码',
+                                    suffixIcon: IconButton(
+                                      onPressed: () {
+                                        // setState(() {
+                                        //   _isObsure = !_isObsure;
+                                        // });
+                                      },
+                                      icon: Icon(
+                                        _isObsure
+                                            ? Icons.remove_red_eye
+                                            : Icons.visibility_off,
                                       ),
-                                      hintText: '请输入密码',
-                                      suffixIcon: IconButton(
-                                        onPressed: () {
-                                          // setState(() {
-                                          //   _isObsure = !_isObsure;
-                                          // });
-                                        },
-                                        icon: Icon(
-                                          _isObsure
-                                              ? Icons.remove_red_eye
-                                              : Icons.visibility_off,
-                                        ),
-                                      )),
-                                ),
-                              ],
-                            ),
+                                    )),
+                              ),
+                            ],
                           ),
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.all(Radius.circular(4)),
-                          ),
-                        )
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            )
-          ],
+                        ),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.all(Radius.circular(4)),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(height: 120),
+              ],
+            ),
+          ),
         ),
         color: Color(0xFF061E33),
       ),
@@ -263,8 +264,10 @@ class MyWidget extends StatelessWidget {
                             color: Colors.white,
                             borderRadius: BorderRadius.all(Radius.circular(4)),
                           ),
-                        )
+                        ),
+
                       ],
+
                     ),
                   )
                 ],
@@ -281,51 +284,49 @@ class MyWidget extends StatelessWidget {
     children: [
       Expanded(
         flex: 3,
-        child: Container(
-          // color: Color(0xFFFFFFFF),
-          child: SvgPicture.asset(
-            'assets/svg/img.svg',
-            height: double.infinity,
-            fit: BoxFit.fill,
-          ),
+        child: SvgPicture.asset(
+          'assets/svg/img.svg',
+          height: double.infinity,
+          fit: BoxFit.fill,
         ),
       ),
       Expanded(
         flex: 2,
         child: Container(
           height: double.infinity,
+          color: const Color(0xFF061E33),
           child: Row(
             children: [
               Container(
                 width: 10,
-                color: Color(0xFF182E42),
+                color: const Color(0xFF182E42),
               ),
               Expanded(
                 child: Column(
                   children: [
-                    SizedBox(height: 120),
+                    const SizedBox(height: 120),
                     Container(
                       height: 6,
-                      color: Color(0xFFCCCCCC),
+                      color: const Color(0xFFCCCCCC),
                     ),
-                    SizedBox(height: 96.2),
-                    Container(
+                    const SizedBox(height: 96.2),
+                    SizedBox(
                       width: 396.58,
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           Row(
                             children: [
-                              SizedBox(width: 15.19),
+                              const SizedBox(width: 15.19),
                               SvgPicture.asset(
                                 'assets/svg/icon_logo.svg',
                                 width: 55.42,
                                 height: 52.31,
                               ),
-                              SizedBox(width: 11.65),
+                              const SizedBox(width: 11.65),
                               Column(
                                 crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
+                                children: const [
                                   Text(
                                     'Audio Frequency',
                                     style: TextStyle(
@@ -345,24 +346,29 @@ class MyWidget extends StatelessWidget {
                               )
                             ],
                           ),
-                          SizedBox(height: 20.78),
+                          const SizedBox(height: 20.78),
                           Container(
                             width: 396.58,
                             height: 349.62,
+                            decoration: const BoxDecoration(
+                              color: Colors.white,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(4)),
+                            ),
                             child: Padding(
                               padding:
                                   const EdgeInsets.fromLTRB(42, 33, 42, 43),
                               child: Column(
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  Text(
+                                  const Text(
                                     '欢迎回来，请登录您的帐户',
                                     style: TextStyle(
                                         color: Color(0xFFA5A6AD), fontSize: 16),
                                   ),
-                                  SizedBox(height: 23.66),
+                                  const SizedBox(height: 23.66),
                                   TextFormField(
-                                    decoration: InputDecoration(
+                                    decoration: const InputDecoration(
                                       label: Text(
                                         '账号',
                                         style: TextStyle(
@@ -372,7 +378,7 @@ class MyWidget extends StatelessWidget {
                                       hintText: '请输入账号',
                                     ),
                                   ),
-                                  SizedBox(height: 38),
+                                  const SizedBox(height: 38),
                                   TextFormField(
                                     obscureText: _isObsure,
                                     inputFormatters: [
@@ -382,7 +388,7 @@ class MyWidget extends StatelessWidget {
                                       ),
                                     ],
                                     decoration: InputDecoration(
-                                        label: Text(
+                                        label: const Text(
                                           '密码',
                                           style: TextStyle(
                                               color: Color(0xFFBBBBBB),
@@ -402,13 +408,13 @@ class MyWidget extends StatelessWidget {
                                           ),
                                         )),
                                   ),
+                                  const SizedBox(height: 30),
+                                  Center(
+                                    child: Container(width: 200,height: 40,alignment:Alignment.center,decoration: BoxDecoration(color: Colors.blue,borderRadius: BorderRadius.circular(20),
+                                    ),child: const Text('Login',style: TextStyle(color: Colors.white),),),
+                                  ),
                                 ],
                               ),
-                            ),
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(4)),
                             ),
                           )
                         ],
@@ -419,11 +425,12 @@ class MyWidget extends StatelessWidget {
               )
             ],
           ),
-          color: Color(0xFF061E33),
         ),
       )
     ],
   );
+
+  MyWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -435,39 +442,20 @@ class MyWidget extends StatelessWidget {
   }
 }
 
-class Responsive extends StatelessWidget {
-  final Widget mobile;
-  final Widget tablet;
-  final Widget desktop;
-  const Responsive({
-    Key? key,
-    required this.desktop,
-    required this.mobile,
-    required this.tablet,
-  }) : super(key: key);
+Future<void> fetchData() async {
+  final httpClient = HttpClient();
 
-  /// mobile < 650
-  static bool isMobile(BuildContext context) =>
-      MediaQuery.of(context).size.width < 650;
+  try {
+    final request = await httpClient.getUrl(Uri.parse('https://example.com/api/data'));
+    final response = await request.close();
 
-  /// tablet >= 650
-  static bool isTablet(BuildContext context) =>
-      MediaQuery.of(context).size.width >= 650;
-
-  ///desktop >= 1100
-  static bool isDesktop(BuildContext context) =>
-      MediaQuery.of(context).size.width >= 1100;
-
-  @override
-  Widget build(BuildContext context) {
-    return LayoutBuilder(builder: (context, constraints) {
-      if (constraints.maxWidth >= 1100) {
-        return desktop;
-      } else if (constraints.maxWidth >= 650) {
-        return tablet;
-      } else {
-        return mobile;
-      }
-    });
+    // Process the response
+  } catch (error) {
+    print(error);
+  } finally {
+    httpClient.close();
   }
 }
+
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Audio Frequency',
       theme: ThemeData(),
-      home: LoginScreen(),
+      home: const LoginScreen(),
     );
   }
 }
